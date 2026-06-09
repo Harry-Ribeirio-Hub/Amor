fde4a9df5e7f97d93c12041042bea7026cac3d4c
Harry <harry.w.ribeiro.s@gmail.com>
2026-06-09 12:01:51 -0400

    Update correct password in script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==========================================
 // CONFIGURAÇÕES GERAIS
 // ==========================================
-const senhaCorreta = "amor2026";
+const senhaCorreta = "62024002roma";
 const dataInicio = "2026-04-20T00:00:00";
 const textoTituloPoema = "❤️ O Nosso Amor Além de Tudo ❤️";
 
